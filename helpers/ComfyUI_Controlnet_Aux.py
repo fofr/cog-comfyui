daa13f3099560139a4615d6317e13b28dfa2a004
Paul <fofr@users.noreply.github.com>
2024-01-18 16:14:56 +0000

    Get all preprocessor weights ourselves

--- helpers/ComfyUI_Controlnet_Aux.py
+++ helpers/ComfyUI_Controlnet_Aux.py
@@ -1,49 +1,85 @@
+# MODELS = {
+#     "150_16_swin_l_oneformer_coco_100ep.pth": "lllyasviel/Annotators",
+#     "250_16_swin_l_oneformer_ade20k_160k.pth": "lllyasviel/Annotators",
+#     "ControlNetHED.pth": "lllyasviel/Annotators",
+#     "ControlNetLama.pth": "lllyasviel/Annotators",
+#     "RealESRGAN_x4plus.pth": "lllyasviel/Annotators",
+#     "ZoeD_M12_N.pt": "lllyasviel/Annotators",
+#     "body_pose_model.pth": "lllyasviel/Annotators",
+#     "clip_g.pth": "lllyasviel/Annotators",
+#     "dpt_hybrid-midas-501f0c75.pt": "lllyasviel/Annotators",
+#     "erika.pth": "lllyasviel/Annotators",
+#     "facenet.pth": "lllyasviel/Annotators",
+#     "hand_pose_model.pth": "lllyasviel/Annotators",
+#     "lama.ckpt": "lllyasviel/Annotators",
+#     "latest_net_G.pth": "lllyasviel/Annotators",
+#     "mlsd_large_512_fp32.pth": "lllyasviel/Annotators",
+#     "netG.pth": "lllyasviel/Annotators",
+#     "network-bsds500.pth": "lllyasviel/Annotators",
+#     "res101.pth": "lllyasviel/Annotators",
+#     "scannet.pt": "lllyasviel/Annotators",
+#     "sk_model.pth": "lllyasviel/Annotators",
+#     "sk_model2.pth": "lllyasviel/Annotators",
+#     "table5_pidinet.pth": "lllyasviel/Annotators",
+#     "upernet_global_small.pth": "lllyasviel/Annotators",
+#     "dw-ll_ucoco_384.onnx": "yzd-v/DWPose",
+#     "yolox_l.onnx": "yzd-v/DWPose",
+#     "dw-ll_ucoco_384_bs5.torchscript.pt": "hr16/DWPose-TorchScript-BatchSize5",
+#     "rtmpose-m_ap10k_256_bs5.torchscript.pt": "hr16/DWPose-TorchScript-BatchSize5",
+#     "yolo_nas_l_fp16.onnx": "hr16/yolo-nas-fp16",
+#     "yolo_nas_m_fp16.onnx": "hr16/yolo-nas-fp16",
+#     "yolo_nas_s_fp16.onnx": "hr16/yolo-nas-fp16",
+#     "yolox_l.torchscript.pt": "hr16/yolox-onnx",
+#     "densepose_r50_fpn_dl.torchscript": "LayerNorm/DensePose-TorchScript-with-hint-image",
+#     "mobile_sam.pt": "dhkim2810/MobileSAM",
+#     "control_sd15_inpaint_depth_hand_fp16.safetensors": "hr16/ControlNet-HandRefiner-pruned",
+#     "graphormer_hand_state_dict.bin": "hr16/ControlNet-HandRefiner-pruned",
+#     "hrnetv2_w64_imagenet_pretrained.pth": "hr16/ControlNet-HandRefiner-pruned",
+#     "isnetis.ckpt": "skytnt/anime-seg",
+#     "dw-ll_ucoco.onnx": "hr16/UnJIT-DWPose",
+#     "dw-ll_ucoco_384_fp16.onnx": "hr16/UnJIT-DWPose",
+#     "dw-ll_ucoco_fp16.onnx": "hr16/UnJIT-DWPose",
+#     "dw-mm_ucoco.onnx": "hr16/UnJIT-DWPose",
+#     "dw-mm_ucoco_fp16.onnx": "hr16/UnJIT-DWPose",
+#     "dw-ss_ucoco.onnx": "hr16/UnJIT-DWPose",
+#     "dw-ss_ucoco_fp16.onnx": "hr16/UnJIT-DWPose",
+#     "rtmpose-m_ap10k_256.onnx": "hr16/UnJIT-DWPose",
+# }
+
 MODELS = {
-    "150_16_swin_l_oneformer_coco_100ep.pth": "lllyasviel/Annotators",
-    "250_16_swin_l_oneformer_ade20k_160k.pth": "lllyasviel/Annotators",
-    "ControlNetHED.pth": "lllyasviel/Annotators",
-    "ControlNetLama.pth": "lllyasviel/Annotators",
-    "RealESRGAN_x4plus.pth": "lllyasviel/Annotators",
-    "ZoeD_M12_N.pt": "lllyasviel/Annotators",
-    "body_pose_model.pth": "lllyasviel/Annotators",
-    "clip_g.pth": "lllyasviel/Annotators",
-    "dpt_hybrid-midas-501f0c75.pt": "lllyasviel/Annotators",
-    "erika.pth": "lllyasviel/Annotators",
-    "facenet.pth": "lllyasviel/Annotators",
-    "hand_pose_model.pth": "lllyasviel/Annotators",
-    "lama.ckpt": "lllyasviel/Annotators",
-    "latest_net_G.pth": "lllyasviel/Annotators",
-    "mlsd_large_512_fp32.pth": "lllyasviel/Annotators",
-    "netG.pth": "lllyasviel/Annotators",
-    "network-bsds500.pth": "lllyasviel/Annotators",
-    "res101.pth": "lllyasviel/Annotators",
-    "scannet.pt": "lllyasviel/Annotators",
-    "sk_model.pth": "lllyasviel/Annotators",
-    "sk_model2.pth": "lllyasviel/Annotators",
-    "table5_pidinet.pth": "lllyasviel/Annotators",
-    "upernet_global_small.pth": "lllyasviel/Annotators",
-    "dw-ll_ucoco_384.onnx": "yzd-v/DWPose",
-    "yolox_l.onnx": "yzd-v/DWPose",
-    "dw-ll_ucoco_384_bs5.torchscript.pt": "hr16/DWPose-TorchScript-BatchSize5",
-    "rtmpose-m_ap10k_256_bs5.torchscript.pt": "hr16/DWPose-TorchScript-BatchSize5",
-    "yolo_nas_l_fp16.onnx": "hr16/yolo-nas-fp16",
-    "yolo_nas_m_fp16.onnx": "hr16/yolo-nas-fp16",
-    "yolo_nas_s_fp16.onnx": "hr16/yolo-nas-fp16",
-    "yolox_l.torchscript.pt": "hr16/yolox-onnx",
-    "densepose_r50_fpn_dl.torchscript": "LayerNorm/DensePose-TorchScript-with-hint-image",
+    "UNet.pth": "bdsqlsz/qinglong_controlnet-lllite/Annotators",
     "mobile_sam.pt": "dhkim2810/MobileSAM",
-    "control_sd15_inpaint_depth_hand_fp16.safetensors": "hr16/ControlNet-HandRefiner-pruned",
-    "graphormer_hand_state_dict.bin": "hr16/ControlNet-HandRefiner-pruned",
     "hrnetv2_w64_imagenet_pretrained.pth": "hr16/ControlNet-HandRefiner-pruned",
-    "isnetis.ckpt": "skytnt/anime-seg",
-    "dw-ll_ucoco.onnx": "hr16/UnJIT-DWPose",
-    "dw-ll_ucoco_384_fp16.onnx": "hr16/UnJIT-DWPose",
-    "dw-ll_ucoco_fp16.onnx": "hr16/UnJIT-DWPose",
-    "dw-mm_ucoco.onnx": "hr16/UnJIT-DWPose",
-    "dw-mm_ucoco_fp16.onnx": "hr16/UnJIT-DWPose",
-    "dw-ss_ucoco.onnx": "hr16/UnJIT-DWPose",
-    "dw-ss_ucoco_fp16.onnx": "hr16/UnJIT-DWPose",
+    "graphormer_hand_state_dict.bin": "hr16/ControlNet-HandRefiner-pruned",
+    "rtmpose-m_ap10k_256_bs5.torchscript.pt": "hr16/DWPose-TorchScript-BatchSize5",
+    "dw-ll_ucoco_384_bs5.torchscript.pt": "hr16/DWPose-TorchScript-BatchSize5",
     "rtmpose-m_ap10k_256.onnx": "hr16/UnJIT-DWPose",
+    "yolo_nas_s_fp16.onnx": "hr16/yolo-nas-fp16",
+    "yolo_nas_m_fp16.onnx": "hr16/yolo-nas-fp16",
+    "yolox_l.torchscript.pt": "hr16/yolox-onnx",
+    "densepose_r101_fpn_dl.torchscript": "LayerNorm/DensePose-TorchScript-with-hint-image",
+    "densepose_r50_fpn_dl.torchscript": "LayerNorm/DensePose-TorchScript-with-hint-image",
+    "mlsd_large_512_fp32.pth": "lllyasviel/Annotators",
+    "150_16_swin_l_oneformer_coco_100ep.pth": "lllyasviel/Annotators",
+    "ControlNetHED.pth": "lllyasviel/Annotators",
+    "ZoeD_M12_N.pt": "lllyasviel/Annotators",
+    "scannet.pt": "lllyasviel/Annotators",
+    "hand_pose_model.pth": "lllyasviel/Annotators",
+    "upernet_global_small.pth": "lllyasviel/Annotators",
+    "latest_net_G.pth": "lllyasviel/Annotators",
+    "netG.pth": "lllyasviel/Annotators",
+    "sk_model2.pth": "lllyasviel/Annotators",
+    "dpt_hybrid-midas-501f0c75.pt": "lllyasviel/Annotators",
+    "table5_pidinet.pth": "lllyasviel/Annotators",
+    "erika.pth": "lllyasviel/Annotators",
+    "250_16_swin_l_oneformer_ade20k_160k.pth": "lllyasviel/Annotators",
+    "sk_model.pth": "lllyasviel/Annotators",
+    "body_pose_model.pth": "lllyasviel/Annotators",
+    "res101.pth": "lllyasviel/Annotators",
+    "facenet.pth": "lllyasviel/Annotators",
+    "isnetis.ckpt": "skytnt/anime-seg",
+    "yolox_l.onnx": "yzd-v/DWPose",
+    "dw-ll_ucoco_384.onnx": "yzd-v/DWPose",
 }
 
 
@@ -66,19 +102,51 @@ class ComfyUI_Controlnet_Aux:
     # We need to add them manually based on the nodes being used to
     # avoid them being downloaded automatically from elsewhere
     @staticmethod
-    def weights_mapping():
+    def node_class_mapping():
         return {
-            "AnimeFace_SemSegPreprocessor": "isnetis.ckpt",
-            "DensePosePreprocessor": "densepose_r50_fpn_dl.torchscript",
+            # Depth
+            "MiDaS-NormalMapPreprocessor": "dpt_hybrid-midas-501f0c75.pt",
+            "MiDaS-DepthMapPreprocessor": "dpt_hybrid-midas-501f0c75.pt",
             "Zoe-DepthMapPreprocessor": "ZoeD_M12_N.pt",
+            "LeReS-DepthMapPreprocessor": ["res101.pth", "latest_net_G.pth"],
+            "MeshGraphormer-DepthMapPreprocessor": [
+                "hrnetv2_w64_imagenet_pretrained.pth",
+                "graphormer_hand_state_dict.bin",
+            ],
+            # Segmentation
+            "BAE-NormalMapPreprocessor": "scannet.pt",
+            "OneFormer-COCO-SemSegPreprocessor": "150_16_swin_l_oneformer_coco_100ep.pth",
+            "OneFormer-ADE20K-SemSegPreprocessor": "250_16_swin_l_oneformer_ade20k_160k.pth",
+            "UniFormer-SemSegPreprocessor": "upernet_global_small.pth",
+            "SemSegPreprocessor": "upernet_global_small.pth",
+            "AnimeFace_SemSegPreprocessor": ["UNet.pth", "isnetis.ckpt"],
+            "SAMPreprocessor": "mobile_sam.pt",
+            # Line extractors
+            "AnimeLineArtPreprocessor": "netG.pth",
+            "HEDPreprocessor": "ControlNetHED.pth",
+            "FakeScribblePreprocessor": "ControlNetHED.pth",
+            "M-LSDPreprocessor": "mlsd_large_512_fp32.pth",
+            "PiDiNetPreprocessor": "table5_pidinet.pth",
+            "LineArtPreprocessor": ["sk_model.pth", "sk_model2.pth"],
+            "Manga2Anime_LineArt_Preprocessor": "erika.pth",
+            # Pose
+            "OpenposePreprocessor": [
+                "body_pose_model.pth",
+                "hand_pose_model.pth",
+                "facenet.pth",
+            ],
         }
 
     @staticmethod
     def add_controlnet_preprocessor_weight(weights_to_download, node):
         if (
             "class_type" in node
-            and node["class_type"] in ComfyUI_Controlnet_Aux.weights_mapping()
+            and node["class_type"] in ComfyUI_Controlnet_Aux.node_class_mapping()
         ):
-            weights_to_download.append(
-                ComfyUI_Controlnet_Aux.weights_mapping()[node["class_type"]]
-            )
+            class_weights = ComfyUI_Controlnet_Aux.node_class_mapping()[
+                node["class_type"]
+            ]
+            if isinstance(class_weights, list):
+                weights_to_download.extend(class_weights)
+            else:
+                weights_to_download.append(class_weights)
