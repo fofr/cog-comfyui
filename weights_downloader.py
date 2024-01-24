1104b88d2bac961779efad9f1daadf06d0f7a643
Paul <fofr@users.noreply.github.com>
2024-01-24 14:14:04 +0000

    Change weights strategy to merge from an updated file

--- weights_downloader.py
+++ weights_downloader.py
@@ -1,75 +1,15 @@
 import subprocess
 import time
 import os
-import json
 
-from helpers.ComfyUI_Controlnet_Aux import ComfyUI_Controlnet_Aux
-from helpers.ComfyUI_AnimateDiff_Evolved import ComfyUI_AnimateDiff_Evolved
+from weights_manifest import WeightsManifest
 
-WEIGHTS_MANIFEST_URL = (
-    f"https://weights.replicate.delivery/default/comfy-ui/weights.json?cache_bypass&time={int(time.time())}"
-)
-WEIGHTS_MANIFEST_PATH = "weights.json"
 BASE_URL = "https://weights.replicate.delivery/default/comfy-ui"
-BASE_PATH = "ComfyUI/models"
 
 
 class WeightsDownloader:
     def __init__(self):
-        self.weights_map = self._initialize_weights_map()
-
-    def _download_weights_manifest(self):
-        manifest_path = os.path.join(WEIGHTS_MANIFEST_PATH)
-        if not os.path.exists(manifest_path):
-            print(f"Downloading weights manifest from {WEIGHTS_MANIFEST_URL}")
-            start = time.time()
-            subprocess.check_call(
-                [
-                    "pget",
-                    "--log-level",
-                    "warn",
-                    "-f",
-                    WEIGHTS_MANIFEST_URL,
-                    manifest_path,
-                ],
-                close_fds=False,
-            )
-            print(
-                f"downloading {WEIGHTS_MANIFEST_URL} took: {(time.time() - start):.2f}s"
-            )
-        else:
-            print("Weights manifest file already exists")
-
-    def _generate_weights_map(self, keys, dest):
-        return {
-            key: {
-                "url": f"{BASE_URL}/{dest}/{key}.tar",
-                "dest": f"{BASE_PATH}/{dest}",
-            }
-            for key in keys
-        }
-
-    def _initialize_weights_map(self):
-        self._download_weights_manifest()
-
-        if not os.path.exists(WEIGHTS_MANIFEST_PATH):
-            raise FileNotFoundError("The weights manifest file does not exist.")
-        with open(WEIGHTS_MANIFEST_PATH, "r") as f:
-            self.weights_manifest = json.load(f)
-
-        weights_map = {}
-        for key in self.weights_manifest.keys():
-            if key.isupper():
-                weights_map.update(
-                    self._generate_weights_map(self.weights_manifest[key], key.lower())
-                )
-        weights_map.update(ComfyUI_Controlnet_Aux.weights_map(BASE_URL))
-        weights_map.update(ComfyUI_AnimateDiff_Evolved.weights_map(BASE_URL))
-        print("Allowed weights:")
-        for weight in weights_map.keys():
-            print(weight)
-
-        return weights_map
+        self.weights_map = WeightsManifest().weights_map
 
     def download_weights(self, weight_str):
         if weight_str in self.weights_map:
@@ -106,35 +46,3 @@ class WeightsDownloader:
         print(
             f"⌛️ Download {weight_str} took: {elapsed_time:.2f}s, size: {file_size_megabytes:.2f}MB"
         )
-
-    def write_supported_weights(self):
-        weight_lists = {
-            "Checkpoints": self.weights_manifest.get("CHECKPOINTS", []),
-            "Upscale models": self.weights_manifest.get("UPSCALE_MODELS", []),
-            "CLIP Vision": self.weights_manifest.get("CLIP_VISION", []),
-            "LORAs": self.weights_manifest.get("LORAS", []),
-            "IPAdapter": self.weights_manifest.get("IPADAPTER", []),
-            "ControlNet": self.weights_manifest.get("CONTROLNET", []),
-            "VAE": self.weights_manifest.get("VAE", []),
-            "PhotoMaker": self.weights_manifest.get("PHOTOMAKER", []),
-            "AnimateDiff": ComfyUI_AnimateDiff_Evolved.models(),
-            "AnimateDiff LORAs": ComfyUI_AnimateDiff_Evolved.loras(),
-            "ControlNet Preprocessors": sorted(
-                {
-                    f"{repo}/{filename}"
-                    for filename, repo in ComfyUI_Controlnet_Aux.models().items()
-                }
-            ),
-        }
-        with open("supported_weights.md", "w") as f:
-            for weight_type, weights in weight_lists.items():
-                f.write(f"## {weight_type}\n\n")
-                for weight in weights:
-                    f.write(f"- {weight}\n")
-                f.write("\n")
-
-    def print_weights_urls(self):
-        weights_urls = [
-            weight_info["url"] for weight_str, weight_info in self.weights_map.items()
-        ]
-        return weights_urls
---
+++ weights_manifest.py
@@ -0,0 +1,118 @@
+import subprocess
+import time
+import os
+import json
+
+from helpers.ComfyUI_Controlnet_Aux import ComfyUI_Controlnet_Aux
+from helpers.ComfyUI_AnimateDiff_Evolved import ComfyUI_AnimateDiff_Evolved
+
+UPDATED_WEIGHTS_MANIFEST_URL = f"https://weights.replicate.delivery/default/comfy-ui/weights.json?cache_bypass&time={int(time.time())}"
+UPDATED_WEIGHTS_MANIFEST_PATH = "updated_weights.json"
+WEIGHTS_MANIFEST_PATH = "weights.json"
+
+BASE_URL = "https://weights.replicate.delivery/default/comfy-ui"
+BASE_PATH = "ComfyUI/models"
+
+
+class WeightsManifest:
+    def __init__(self):
+        self.weights_manifest = self._load_weights_manifest()
+        self.weights_map = self._initialize_weights_map()
+
+    def _load_weights_manifest(self):
+        self._download_updated_weights_manifest()
+        return self._merge_manifests()
+
+    def _download_updated_weights_manifest(self):
+        if not os.path.exists(UPDATED_WEIGHTS_MANIFEST_PATH):
+            print(
+                f"Downloading updated weights manifest from {UPDATED_WEIGHTS_MANIFEST_URL}"
+            )
+            start = time.time()
+            subprocess.check_call(
+                [
+                    "pget",
+                    "--log-level",
+                    "warn",
+                    "-f",
+                    UPDATED_WEIGHTS_MANIFEST_URL,
+                    UPDATED_WEIGHTS_MANIFEST_PATH,
+                ],
+                close_fds=False,
+            )
+            print(
+                f"Downloading {UPDATED_WEIGHTS_MANIFEST_URL} took: {(time.time() - start):.2f}s"
+            )
+        else:
+            print("Updated weights manifest file already exists")
+
+    def _merge_manifests(self):
+        if os.path.exists(WEIGHTS_MANIFEST_PATH):
+            with open(WEIGHTS_MANIFEST_PATH, "r") as f:
+                original_manifest = json.load(f)
+        else:
+            original_manifest = {}
+
+        with open(UPDATED_WEIGHTS_MANIFEST_PATH, "r") as f:
+            updated_manifest = json.load(f)
+
+        for key in updated_manifest:
+            if key in original_manifest:
+                for item in updated_manifest[key]:
+                    if item not in original_manifest[key]:
+                        print(f"Adding {item} to {key}")
+                        original_manifest[key].append(item)
+            else:
+                original_manifest[key] = updated_manifest[key]
+
+        return original_manifest
+
+    def _generate_weights_map(self, keys, dest):
+        return {
+            key: {
+                "url": f"{BASE_URL}/{dest}/{key}.tar",
+                "dest": f"{BASE_PATH}/{dest}",
+            }
+            for key in keys
+        }
+
+    def _initialize_weights_map(self):
+        weights_map = {}
+        for key in self.weights_manifest.keys():
+            if key.isupper():
+                weights_map.update(
+                    self._generate_weights_map(self.weights_manifest[key], key.lower())
+                )
+        weights_map.update(ComfyUI_Controlnet_Aux.weights_map(BASE_URL))
+        weights_map.update(ComfyUI_AnimateDiff_Evolved.weights_map(BASE_URL))
+        print("Allowed weights:")
+        for weight in weights_map.keys():
+            print(weight)
+
+        return weights_map
+
+    def write_supported_weights(self):
+        weight_lists = {
+            "Checkpoints": self.weights_manifest.get("CHECKPOINTS", []),
+            "Upscale models": self.weights_manifest.get("UPSCALE_MODELS", []),
+            "CLIP Vision": self.weights_manifest.get("CLIP_VISION", []),
+            "LORAs": self.weights_manifest.get("LORAS", []),
+            "IPAdapter": self.weights_manifest.get("IPADAPTER", []),
+            "ControlNet": self.weights_manifest.get("CONTROLNET", []),
+            "VAE": self.weights_manifest.get("VAE", []),
+            "PhotoMaker": self.weights_manifest.get("PHOTOMAKER", []),
+            "AnimateDiff": ComfyUI_AnimateDiff_Evolved.models(),
+            "AnimateDiff LORAs": ComfyUI_AnimateDiff_Evolved.loras(),
+            "ControlNet Preprocessors": sorted(
+                {
+                    f"{repo}/{filename}"
+                    for filename, repo in ComfyUI_Controlnet_Aux.models().items()
+                }
+            ),
+        }
+        with open("supported_weights.md", "w") as f:
+            for weight_type, weights in weight_lists.items():
+                f.write(f"## {weight_type}\n\n")
+                for weight in weights:
+                    f.write(f"- {weight}\n")
+                f.write("\n")
