99dda6ad30e48a2a5c8436ef3bc5fc41f923debf
Paul <fofr@users.noreply.github.com>
2024-01-29 09:44:54 +0000

    Add artificialguybr loras. Support nested folders in downloads

--- weights_downloader.py
+++ weights_downloader.py
@@ -35,14 +35,21 @@ class WeightsDownloader:
             self.download(weight_str, url, dest)
 
     def download(self, weight_str, url, dest):
-        print(f"⏳ Downloading {weight_str}")
+        if "/" in weight_str:
+            subfolder = weight_str.rsplit("/", 1)[0]
+            dest = os.path.join(dest, subfolder)
+            os.makedirs(dest, exist_ok=True)
+
+        print(f"⏳ Downloading {weight_str} to {dest}")
         start = time.time()
         subprocess.check_call(
             ["pget", "--log-level", "warn", "-xf", url, dest], close_fds=False
         )
         elapsed_time = time.time() - start
-        file_size_bytes = os.path.getsize(f"{dest}/{weight_str}")
+        file_size_bytes = os.path.getsize(
+            os.path.join(dest, os.path.basename(weight_str))
+        )
         file_size_megabytes = file_size_bytes / (1024 * 1024)
         print(
-            f"⌛️ Download {weight_str} took: {elapsed_time:.2f}s, size: {file_size_megabytes:.2f}MB"
+            f"⌛️ Downloaded {weight_str} in {elapsed_time:.2f}s, size: {file_size_megabytes:.2f}MB"
         )
