433b449156b1f41ee4c7f8fe232cc689433fc1e5
Paul <fofr@users.noreply.github.com>
2024-02-08 16:32:53 +0000

    Allow passing of a text file to get weights

--- scripts/get_weights.py
+++ scripts/get_weights.py
@@ -3,13 +3,24 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from weights_downloader import WeightsDownloader
 
-def main(filename):
+def download_weight_files(weight_files):
     wd = WeightsDownloader()
-    wd.download_weights(filename)
+    for weight_file in weight_files:
+        wd.download_weights(weight_file)
+
+def main(filenames):
+    weight_files = []
+    for filename in filenames:
+        if filename.endswith('.txt'):
+            with open(filename, 'r') as f:
+                weight_files.extend(f.read().splitlines())
+        else:
+            weight_files.append(filename)
+    download_weight_files(weight_files)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python get_weights.py <filename>")
+    if len(sys.argv) < 2:
+        print("Usage: python get_weights.py <filename> [<filename> ...] or python get_weights.py <weights.txt>")
         sys.exit(1)
-    filename = sys.argv[1]
-    main(filename)
+    filenames = sys.argv[1:]
+    main(filenames)
