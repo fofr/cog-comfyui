7847b6c4d7d6aae9b4a134e2b945706ad3c893e0
John Richard Chipps-Harding <mrharding@gmail.com>
2024-02-12 22:06:58 +0000

    Disable Metadata
    Disable metadata embedding on generated images

--- helpers/comfyui.py
+++ helpers/comfyui.py
@@ -44,7 +44,7 @@ class ComfyUI:
         print("Server running")
 
     def run_server(self, output_directory, input_directory):
-        command = f"python ./ComfyUI/main.py --output-directory {output_directory} --input-directory {input_directory}"
+        command = f"python ./ComfyUI/main.py --output-directory {output_directory} --input-directory {input_directory} --disable-metadata"
         server_process = subprocess.Popen(command, shell=True)
         server_process.wait()
 
