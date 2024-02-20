e5aa49909182d4501c7e3767858fb9115b240985
Paul <fofr@users.noreply.github.com>
2024-02-20 15:03:07 +0000

    Add SAM and GroundingDino weights

--- weights_manifest.py
+++ weights_manifest.py
@@ -126,6 +126,7 @@ class WeightsManifest:
             "InsightFace": self.weights_manifest.get("INSIGHTFACE", []),
             "Ultralytics": self.weights_manifest.get("ULTRALYTICS", []),
             "Segment anything models (SAM)": self.weights_manifest.get("SAMS", []),
+            "GroundingDino": self.weights_manifest.get("GROUNDING-DINO", []),
             "MMDets": self.weights_manifest.get("MMDETS", []),
             "Face restoration models": self.weights_manifest.get(
                 "FACERESTORE_MODELS", []
