3f72ccae2e2d4c4f9e77983e90c93dee173caae8
Paul <fofr@users.noreply.github.com>
2024-04-17 09:09:39 +0000

    Make sort_weights executable

--- scripts/sort_weights.py
+++ scripts/sort_weights.py
@@ -1,21 +1,28 @@
+#!/usr/bin/env python3
+
 import json
 import os
 import sys
 
+# Ensure the script can import WeightsManifest from the parent directory
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from weights_manifest import WeightsManifest
 
-# Load the JSON data from a file
-with open('weights.json', 'r') as file:
-    data = json.load(file)
+def main():
+    # Load the JSON data from a file
+    with open('weights.json', 'r') as file:
+        data = json.load(file)
 
-# Sort each array in the JSON data
-for key in data:
-    if isinstance(data[key], list):
-        data[key].sort(key=str.casefold)
+    # Sort each array in the JSON data
+    for key in data:
+        if isinstance(data[key], list):
+            data[key].sort(key=str.casefold)
 
-# Write the sorted JSON data back to the file
-with open('weights.json', 'w') as file:
-    json.dump(data, file, indent=2)
+    # Write the sorted JSON data back to the file
+    with open('weights.json', 'w') as file:
+        json.dump(data, file, indent=2)
 
-WeightsManifest().write_supported_weights()
+    WeightsManifest().write_supported_weights()
+
+if __name__ == "__main__":
+    main()
