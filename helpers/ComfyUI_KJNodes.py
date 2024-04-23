3e4eca2659956a04eada2574989f418a73ce11bf
Paul <fofr@users.noreply.github.com>
2024-04-23 11:50:14 +0000

    Add KJNodes

---
+++ helpers/ComfyUI_KJNodes.py
@@ -0,0 +1,15 @@
+class ComfyUI_KJNodes:
+    @staticmethod
+    def add_weights(weights_to_download, node):
+        if node.get("class_type") == "BatchCLIPSeg":
+            weights_to_download.extend(["models--CIDAS--clipseg-rd64-refined"])
+
+    @staticmethod
+    def check_for_unsupported_nodes(node):
+        unsupported_nodes = {
+            "Superprompt": "Superprompt is not supported as it needs to download T5 weights",
+        }
+        node_class = node.get("class_type")
+        if node_class in unsupported_nodes:
+            reason = unsupported_nodes[node_class]
+            raise ValueError(f"{node_class} node is not supported: {reason}")
