1adb65f0f7546781c8322157caee837a067a6a3e
Paul <fofr@users.noreply.github.com>
2024-04-23 12:10:17 +0000

    Disable SD API node

--- helpers/ComfyUI_KJNodes.py
+++ helpers/ComfyUI_KJNodes.py
@@ -7,6 +7,7 @@ class ComfyUI_KJNodes:
     @staticmethod
     def check_for_unsupported_nodes(node):
         unsupported_nodes = {
+            "StabilityAPI_SD3": "Calling an external API and passing your key is not supported and is unsafe",
             "Superprompt": "Superprompt is not supported as it needs to download T5 weights",
         }
         node_class = node.get("class_type")
