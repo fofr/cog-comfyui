de6ff648431d180c50b26336d4508849b51e6d5d
Paul <fofr@users.noreply.github.com>
2024-05-03 12:30:53 +0000

    Fix tineterraNodes name check

--- custom_node_helpers/ComfyUI_tinyterraNodes.py
+++ custom_node_helpers/ComfyUI_tinyterraNodes.py
@@ -4,7 +4,7 @@ from custom_node_helper import CustomNodeHelper
 class ComfyUI_tinyterraNodes(CustomNodeHelper):
     @staticmethod
     def check_for_unsupported_nodes(node):
-        if node.is_type("imageREMBG"):
+        if node.is_type("ttN imageREMBG"):
             raise ValueError(
                 "imageREMBG node is not supported in tinyterraNodes. Recommend using RemBGSession from ComfyUI_Essentials"
             )
