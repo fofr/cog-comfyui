1db67435be5f7fae6732cc1b803adc46b86c3b66
Paul <fofr@users.noreply.github.com>
2024-06-01 21:52:42 +0000

    Update KJNodes

--- custom_node_helpers/ComfyUI_KJNodes.py
+++ custom_node_helpers/ComfyUI_KJNodes.py
@@ -3,7 +3,7 @@ from custom_node_helper import CustomNodeHelper
 class ComfyUI_KJNodes(CustomNodeHelper):
     @staticmethod
     def add_weights(weights_to_download, node):
-        if node.is_type("BatchCLIPSeg"):
+        if node.is_type_in(["BatchCLIPSeg", "DownloadAndLoadCLIPSeg"]):
             weights_to_download.extend(["models--CIDAS--clipseg-rd64-refined"])
 
     @staticmethod
