b2dc3b5982c189b8b83e1bbc328aa18c3a47f1e6
Paul <fofr@users.noreply.github.com>
2024-07-08 14:18:11 +0000

    Add Kolors, fix LLM directory

--- scripts/sort_weights.py
+++ scripts/sort_weights.py
@@ -39,6 +39,7 @@ def write_supported_weights():
         "REMBG (Remove background)": weights_manifest.get_weights_by_type("REMBG"),
         "PuLID": weights_manifest.get_weights_by_type("PULID"),
         "GLIGEN": weights_manifest.get_weights_by_type("GLIGEN"),
+        "Diffusers": weights_manifest.get_weights_by_type("DIFFUSERS"),
         "Language models": weights_manifest.get_weights_by_type("LLM"),
         "Anyline": helpers.ComfyUI_Anyline.models(),
         "AnimateDiff": weights_manifest.get_weights_by_type("ANIMATEDIFF_MODELS"),
