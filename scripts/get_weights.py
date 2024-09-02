3032defd0f95ad3d0501e8728a7b13fb7538e99a
fofr <fofr@users.noreply.github.com>
2024-09-02 15:37:47 +0000

    Add predefined sets of weights

--- scripts/get_weights.py
+++ scripts/get_weights.py
@@ -10,8 +10,19 @@ to handle the actual downloading of the weight files.
 import sys
 import os
 import json
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from weights_downloader import WeightsDownloader
+
+PREDEFINED_WEIGHT_SETS = {
+    "flux": [
+        "flux1-dev.safetensors",
+        "ae.safetensors",
+        "clip_l.safetensors",
+        "t5xxl_fp8_e4m3fn.safetensors",
+    ]
+}
+
 
 def download_weight_files(weight_files):
     wd = WeightsDownloader()
@@ -22,32 +33,41 @@ def download_weight_files(weight_files):
             print(f"Failed to download {weight_file}: {str(e)}")
             continue
 
+
 def extract_weights_from_workflow(workflow_path):
-    with open(workflow_path, 'r') as f:
+    with open(workflow_path, "r") as f:
         workflow = json.load(f)
     weights_to_download = []
     for node in workflow.values():
         if "inputs" in node:
             for input in node["inputs"].values():
-                if isinstance(input, str) and input.endswith(tuple(WeightsDownloader.supported_filetypes)):
+                if isinstance(input, str) and input.endswith(
+                    tuple(WeightsDownloader.supported_filetypes)
+                ):
                     weights_to_download.append(input)
     return list(set(weights_to_download))
+
 
 def main(filenames):
     weight_files = []
     for filename in filenames:
-        if filename.endswith('.txt'):
-            with open(filename, 'r') as f:
+        if filename in PREDEFINED_WEIGHT_SETS:
+            weight_files.extend(PREDEFINED_WEIGHT_SETS[filename])
+        elif filename.endswith(".txt"):
+            with open(filename, "r") as f:
                 weight_files.extend(f.read().splitlines())
-        elif filename.endswith('.json'):
+        elif filename.endswith(".json"):
             weight_files.extend(extract_weights_from_workflow(filename))
         else:
             weight_files.append(filename)
     download_weight_files(weight_files)
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python get_weights.py <filename> [<filename> ...] or python get_weights.py <weights.txt> or python get_weights.py <workflow.json>")
+        print(
+            "Usage: python get_weights.py <filename> [<filename> ...] or python get_weights.py <weights.txt> or python get_weights.py <workflow.json>"
+        )
         sys.exit(1)
     filenames = sys.argv[1:]
     main(filenames)
