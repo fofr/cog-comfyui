841ff044f888fd146956df5b9670fffb9d010515
fofr <fofr@users.noreply.github.com>
2025-01-28 15:23:20 +0000

    Update InstantID example

--- predict.py
+++ predict.py
@@ -25,7 +25,7 @@ ALL_DIRECTORIES = [OUTPUT_DIR, INPUT_DIR, COMFYUI_TEMP_OUTPUT_DIR]
 IMAGE_TYPES = [".jpg", ".jpeg", ".png", ".webp"]
 VIDEO_TYPES = [".mp4", ".mov", ".avi", ".mkv"]
 
-with open("examples/api_workflows/glif_custom_lora_flux_hf.json", "r") as file:
+with open("examples/api_workflows/instantid_default_api.json", "r") as file:
     EXAMPLE_WORKFLOW_JSON = file.read()
 
 
